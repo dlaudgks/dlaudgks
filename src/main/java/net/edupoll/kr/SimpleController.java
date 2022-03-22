c713979c2a251ea9a468f0c29c190e313e7820d6
dlaudgks <dlaudgks99917@gmail.com>
2022-03-22 10:08:28 +0900

    @Controller

--- src/main/java/net/edupoll/kr/SimpleController.java
+++ src/main/java/net/edupoll/kr/SimpleController.java
@@ -1,6 +1,13 @@
 package net.edupoll.kr;
 
+import org.springframework.stereotype.Controller;
+
+@Controller
 public class SimpleController {
  
+	public String defaultHandle() {
+		
+		return "home";
+	}
 	
 }
